9da5e86be6a5cbf25987f6ba4897e565dbd296c1
VitorCheung <Vitorcheung@gmail.com>
2022-09-02 16:34:53 -0300

    Projeto criado

---
+++ Macro/Macro/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Macro
+//
+//  Created by Vitor Cheung on 02/09/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
