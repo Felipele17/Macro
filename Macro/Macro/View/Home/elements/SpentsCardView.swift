c05a7f1a7b9ebdb5f1b951af3cb5fea3bb34985c
VitorCheung <Vitorcheung@gmail.com>
2022-09-12 17:49:16 -0300

    estrutura feita

--- Macro/Macro/View/Home/elements/SpentsCardView.swift
+++ Macro/Macro/View/Home/elements/SpentsCardView.swift
@@ -7,7 +7,7 @@
 
 import SwiftUI
 
-struct SpentsCardView: View {
+struct SpentsCardView: View{
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
--- Macro/Macro/View/Spent/SpentView.swift
+++ Macro/Macro/View/Spent/SpentView.swift
@@ -9,7 +9,48 @@ import SwiftUI
 
 struct SpentView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationView {
+            VStack {
+                Text("Gasto atual")
+                    .font(.title)
+                    .bold()
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                    .padding([.leading, .top])
+                Text("R$ 1470,00")
+                    .font(.largeTitle)
+                    .bold()
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                    .padding(.leading)
+                Text("Limite disponivel R$ 530,00")
+                    .font(.system(size: 20, weight: .light, design: .default))
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                    .padding()
+                HStack {
+                    Text("Gasto Essenciais")
+                        .font(.largeTitle)
+                        .bold()
+                        .frame(maxWidth: .infinity, alignment: .leading)
+                        .padding(.leading)
+                    Spacer()
+                    Button {
+                        print("add gasto")
+                    } label: {
+                        Label("", systemImage: "plus")
+                            .padding(.trailing)
+                    }
+
+                }
+                List {
+                    SpentsDetailsCardView()
+                    SpentsDetailsCardView()
+                    SpentsDetailsCardView()
+                    SpentsDetailsCardView()
+                }
+            }
+            .navigationTitle("Essencial")
+            .navigationBarTitleDisplayMode(.inline)
+            
+        }
     }
 }
 
