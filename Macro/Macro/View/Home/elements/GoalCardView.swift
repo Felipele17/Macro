d49ad20bcefd5ef0c4b3e545317974aa7abaeadc
Gabriele Namie <gabrielenamie@hotmail.com>
2022-09-19 16:49:40 -0300

    config card

--- Macro/Macro/View/Home/elements/GoalCardView.swift
+++ Macro/Macro/View/Home/elements/GoalCardView.swift
@@ -9,16 +9,34 @@ import SwiftUI
 
 struct GoalCardView: View, Identifiable {
     var id: Int
+    let goal: Goal
     var body: some View {
-        ZStack {
-            Color.blue
-            Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        HStack {
+            VStack(alignment: .leading) {
+                Text(goal.title)
+                    .font(.title2).bold()
+                    .padding(.bottom, 2)
+                
+   //             Text("Motivação:").font(.footnote).bold() + Text(goal.motivation).font(.footnote)
+                
+                Text("R$\(goal.value/10) ")
+                    .font(.body).bold()
+                    + Text("de R$\(goal.value)").font(.body .weight(.light))
+                    
+                Spacer()
+            }
+            .padding(.leading, 20)
+              .foregroundColor(.white)
+              .padding(.top, 20)
+//            goal.priority
+//                .padding(.top)
+            Spacer()
         }
     }
 }
 
-struct GoalCardView_Previews: PreviewProvider {
-    static var previews: some View {
-        GoalCardView(id: 0)
-    }
-}
+//struct GoalCardView_Previews: PreviewProvider {
+//    static var previews: some View {
+//    GoalCardView(id: 1, goal: Goal(title: "META", motivation: "Quero guardar dindin", priority: Image("Nozes"), value: 20, check: 900, category: .work))
+  //  }
+//}
