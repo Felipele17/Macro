5991a5d9e78e4eec889f89feb572c83173bc3a1e
VitorCheung <Vitorcheung@gmail.com>
2022-09-23 16:27:38 -0300

    button

---
+++ Macro/Macro/View/elements/NextButton.swift
@@ -0,0 +1,33 @@
+//
+//  NextButton.swift
+//  Macro
+//
+//  Created by Vitor Cheung on 23/09/22.
+//
+
+import Foundation
+import SwiftUI
+
+struct NextButton: View {
+    
+    var text: String
+    @Binding var onboardingPage: Int
+    @Binding var income: Float
+    
+    var body: some View {
+        Button {
+            if onboardingPage != 3 {
+                onboardingPage += 1
+            }
+            
+        } label: {
+            Text(text)
+                .font(.headline)
+                .foregroundColor(.white)
+                .frame(height: 55)
+                .frame(maxWidth: .infinity)
+                .background(income != 0.0 ? Color("Button") : Color("Placeholder"))
+                .cornerRadius(13)
+        }
+    }
+}
