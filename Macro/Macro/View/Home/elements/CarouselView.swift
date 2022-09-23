e3ff46476c8f27476265ba28651f981adb324d7d
Gabriele Namie <gabrielenamie@hotmail.com>
2022-09-23 14:29:27 -0300

    bug resolvido

--- Macro/Macro/View/Home/elements/CarouselView.swift
+++ Macro/Macro/View/Home/elements/CarouselView.swift
@@ -9,35 +9,28 @@ import SwiftUI
 
 struct CarouselView: View {
     
-    @ObservedObject var viewModel: CarouselViewModel
-    
+    let width: CGFloat
+    let heigth: CGFloat
     var viewsCells: [GoalCardView]
     
     var body: some View {
-        ZStack {
-            ForEach(viewsCells) { cell in
-                NavigationLink(destination: GoalsView(goal: cell.goal)) {
-                    cell
-                        .frame(width: viewModel.width, height: viewModel.heigth)
-                        .offset(x: viewModel.myXOffset(cell.id), y: 0)
+        ScrollView(.horizontal) {
+            HStack {
+                ForEach(viewsCells) { cell in
+                    NavigationLink(destination: GoalsView(goal: cell.goal)) {
+                        cell
+                            .frame(width: width, height: heigth)
+                            .padding([.leading, .bottom])
+                    }
                 }
             }
         }
-        .highPriorityGesture(
-            DragGesture()
-                .onChanged { value in
-                    viewModel.onChange(value: value.translation.width)
-                }
-                .onEnded {value in
-                    viewModel.onEnded(value: value.translation.width, viewsCellsCount: viewsCells.count)
-                }
-        )
     }
 }
 
 struct CarouselView_Previews: PreviewProvider {
     static var previews: some View {
-        CarouselView(viewModel: CarouselViewModel( width: 325.0, heigth: 200.0), viewsCells: [
+        CarouselView (width: 325.0, heigth: 200.0, viewsCells: [
             GoalCardView(viewModel: GoalCardViewModel(), id: 1, goal: Goal(title: "Carro Novo", value: 20000, weeks: 48, motivation: "Realização de um sonho", priority: 1, methodologyGoal: MethodologyGoal(weeks: 52, crescent: true))),
             GoalCardView(viewModel: GoalCardViewModel(), id: 2, goal: Goal(title: "Carro Novo", value: 20000, weeks: 48, motivation: "Realização de um sonho", priority: 1, methodologyGoal: MethodologyGoal(weeks: 52, crescent: true))),
             GoalCardView(viewModel: GoalCardViewModel(), id: 3, goal: Goal(title: "Carro Novo", value: 20000, weeks: 48, motivation: "Realização de um sonho", priority: 1, methodologyGoal: MethodologyGoal(weeks: 52, crescent: true)))
