47cd18b2c7b628b0584c29940ac471d963b31fab
Bianca Maciel <61274369+BiancaMMatos@users.noreply.github.com>
2022-09-26 16:21:45 -0300

    model

---
+++ Macro/Macro/Model/Enum/EnumOnBoardingText.swift
@@ -0,0 +1,8 @@
+//
+//  EnumOnBoardingText.swift
+//  Macro
+//
+//  Created by Bianca Maciel Matos on 26/09/22.
+//
+
+import Foundation
---
+++ Macro/Macro/Model/OnBoarding.swift
@@ -0,0 +1,23 @@
+//
+//  OnBoarding.swift
+//  Macro
+//
+//  Created by Bianca Maciel Matos on 26/09/22.
+//
+
+import SwiftUI
+
+struct OnBoarding: Identifiable, Hashable, Equatable {
+    let id = UUID()
+    var imageName: String
+    var title: String
+    var description: String
+    var tag: Int
+    
+    static var onboardingPages: [OnBoarding] = [
+        OnBoarding(imageName: ImageName.nuts.rawValue, title: OnBoardingText.titleFirstScreen.rawValue, description: OnBoardingText.firstScreen.rawValue, tag: 0),
+        OnBoarding(imageName: ImageName.squirrels.rawValue, title: OnBoardingText.titleSecondScreen.rawValue, description: OnBoardingText.secondScreen.rawValue, tag: 1),
+        OnBoarding(imageName: ImageName.squirrel.rawValue, title: OnBoardingText.titleIncomeScreen.rawValue, description: OnBoardingText.incomeScreen.rawValue, tag: 2),
+        OnBoarding(imageName: ImageName.invitePhone.rawValue, title: OnBoardingText.titleInviteScreen.rawValue, description: OnBoardingText.inviteScreen.rawValue, tag: 3)
+    ]
+}
