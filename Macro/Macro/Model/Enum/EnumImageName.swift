8f212f2ac13ae05f55314dde3ea8eeb217cc6ffb
Bianca Maciel <61274369+BiancaMMatos@users.noreply.github.com>
2022-09-26 16:27:38 -0300

    fim wu acho

---
+++ Macro/Macro/Model/Enum/EnumImageName.swift
@@ -0,0 +1,15 @@
+//
+//  EnumImage.swift
+//  Macro
+//
+//  Created by Bianca Maciel Matos on 26/09/22.
+//
+
+import Foundation
+
+enum EnumImageName: String {
+    case nuts = "noz"
+    case squirrels = "esquilos"
+    case squirrel = "esquilo"
+    case invitePhone = "celular"
+}
