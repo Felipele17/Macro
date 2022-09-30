2bdae9b891c4a0abd7f807e436c55f7d75d29419
Felipe Torales Leite <felipetolei@hotmail.com>
2022-09-30 16:21:39 -0300

    Tentativa de correção do espaçamento

--- Macro/Macro/View/Spent/SpentView.swift
+++ Macro/Macro/View/Spent/SpentView.swift
@@ -9,55 +9,57 @@ import SwiftUI
 
 struct SpentView: View {
     var body: some View {
-            VStack {
-                Text("Gasto atual")
-                    .font(.custom(EnumFonts.bold.rawValue, size: 22))
-                    .frame(maxWidth: .infinity, alignment: .leading)
-                    .padding([.leading, .top])
-                Text("R$ 1470,00")
+        VStack (alignment: .leading){
+            Text("Gasto atual")
+                .font(.custom(EnumFonts.bold.rawValue, size: 22))
+            //.frame(maxWidth: .infinity, alignment: .leading)
+            //.padding([.leading, .top])
+            Text("R$ 1470,00")
+                .font(.custom(EnumFonts.bold.rawValue, size: 28))
+            //.frame(maxWidth: .infinity, alignment: .leading)
+            //.padding(.leading)
+            Text("Limite disponivel R$ 530,00")
+                .font(.custom(EnumFonts.regular.rawValue, size: 17))
+            //.frame(maxWidth: .infinity, alignment: .leading)
+            //.padding(.top)
+            HStack {
+                Text("Gasto Essenciais")
                     .font(.custom(EnumFonts.bold.rawValue, size: 28))
-                    .frame(maxWidth: .infinity, alignment: .leading)
-                    .padding(.leading)
-                Text("Limite disponivel R$ 530,00")
-                    .font(.custom(EnumFonts.regular.rawValue, size: 17))
-                    .frame(maxWidth: .infinity, alignment: .leading)
-                    .padding()
-                HStack {
-                    Text("Gasto Essenciais")
-                        .font(.custom(EnumFonts.bold.rawValue, size: 28))
-                    Button {
-                        print("add gasto")
-                    } label: {
-                        Label("", systemImage: "plus")
-                            .foregroundColor(Color(EnumColors.ButtonColor.rawValue))
-                            .padding(.leading, 48)
-                    }
-
+                Button {
+                    print("add gasto")
+                } label: {
+                    Label("", systemImage: "plus")
+                        .foregroundColor(Color(EnumColors.ButtonColor.rawValue))
+                    //.padding(.leading, 48)
                 }
-                .frame(maxWidth: .infinity, alignment: .leading)
-                .padding(.leading)
-                List {
-                    SpentsDetailsCardView()
-                    SpentsDetailsCardView()
-                    SpentsDetailsCardView()
-                    SpentsDetailsCardView()
-                }
-                .padding(.trailing)
-                .offset(y: -24)
+                
             }
-            .navigationTitle("Essencial")
-            .font(.custom(EnumFonts.semibold.rawValue, size: 17))
-            .navigationBarTitleDisplayMode(.inline)
-//            .toolbar {
-//                Button(role: nil) {
-//                    print("editar")
-//                } label: {
-//                    Text("editar")
-//                        .font(.custom(EnumFonts.regular.rawValue, size: 17))
-//                        .tint(.blue)
-//                }
-//            }
+            //.frame(maxWidth: .infinity, alignment: .leading)
+            //.padding(.leading)
+            List {
+                SpentsDetailsCardView()
+                SpentsDetailsCardView()
+                SpentsDetailsCardView()
+                SpentsDetailsCardView()
+            }
+            .padding(.trailing)
+        }
+        .padding(.leading, 20)
+        .navigationTitle("Essencial")
+        .font(.custom(EnumFonts.semibold.rawValue, size: 17))
+        .navigationBarTitleDisplayMode(.inline)
+        .background(Color(EnumColors.ButtonUnselect.rawValue))
+        //            .toolbar {
+        //                Button(role: nil) {
+        //                    print("editar")
+        //                } label: {
+        //                    Text("editar")
+        //                        .font(.custom(EnumFonts.regular.rawValue, size: 17))
+        //                        .tint(.blue)
+        //                }
+        //            }
     }
+        
 }
 
 struct SpentView_Previews: PreviewProvider {
