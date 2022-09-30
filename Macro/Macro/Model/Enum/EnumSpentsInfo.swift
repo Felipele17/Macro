070bf48f180f0b7cdb9009267b28a3a05e54161a
Gabriele Namie <gabrielenamie@hotmail.com>
2022-09-30 19:39:27 -0300

    estrutura cards gastos

---
+++ Macro/Macro/Model/Enum/EnumSpentsInfo.swift
@@ -0,0 +1,19 @@
+//
+//  EnumSpentsInfo.swift
+//  Macro
+//
+//  Created by Gabriele Namie on 30/09/22.
+//
+
+import SwiftUI
+
+enum EnumSpentsinfo: String {
+    case essencialsColor = "EssenciaisColor"
+    case priorityColor = "PrioridadesColor"
+    case leisureColor = "LazerColor"
+    case backgroundSpentsColor = "BackgroundSpents"
+    case essencialsTitle = "Essenciais"
+    case priorityTitle = "Prioridades"
+    case leisureTitle = "Lazer"
+    
+}
