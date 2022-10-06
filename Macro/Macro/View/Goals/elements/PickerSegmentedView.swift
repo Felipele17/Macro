631418fa49fb2b3f4a178b4c01ef4c5701bd9666
Gabriele Namie <gabrielenamie@hotmail.com>
2022-10-06 17:35:01 -0300

    finalização tela goals

---
+++ Macro/Macro/View/Goals/elements/PickerSegmentedView.swift
@@ -0,0 +1,37 @@
+//
+//  PickerSegmentedView.swift
+//  Macro
+//
+//  Created by Gabriele Namie on 06/10/22.
+//
+
+import SwiftUI
+
+struct PickerSegmentedView: View {
+    @Binding var selectFilter: Int
+    init(selectFilter: Binding<Int>) {
+        UISegmentedControl.appearance().selectedSegmentTintColor = UIColor(Color(EnumColors.buttonColor.rawValue))
+           UISegmentedControl.appearance().setTitleTextAttributes([.foregroundColor: UIColor.white], for: .selected)
+           UISegmentedControl.appearance().setTitleTextAttributes([.foregroundColor: UIColor.black], for: .normal)
+        self._selectFilter = selectFilter
+    }
+    var body: some View {
+        Picker("Qual filtro voce?", selection: $selectFilter) {
+            Text("Todos").tag(0)
+                .font(.custom(EnumFonts.medium.rawValue, size: 13))
+            Text("À depositar").tag(1)
+                .font(.custom(EnumFonts.medium.rawValue, size: 13))
+                
+            Text("Depositado").tag(2)
+                .font(.custom(EnumFonts.medium.rawValue, size: 13))
+        }
+        .pickerStyle(.segmented)
+        .padding([.leading, .trailing, .top])
+    }
+}
+
+struct PickerSegmentedView_Previews: PreviewProvider {
+    static var previews: some View {
+        PickerSegmentedView(selectFilter: .constant(3))
+    }
+}
