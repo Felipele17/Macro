2f2fdd23d70611f6ada42ca0c422894d00f9bdcd
Gabriele Namie <gabrielenamie@hotmail.com>
2022-10-07 15:35:34 -0300

    vetor sfsymbols

--- Macro/Macro/View/Formulary/ModalView.swift
+++ Macro/Macro/View/Formulary/ModalView.swift
@@ -7,25 +7,39 @@
 
 import SwiftUI
 
-s
-
 struct ModalView: View {
+    @State var icons = ["car.fill", "books.vertical.fill", "house.fill", "iphone", "airplane", "pawprint.fill", "cart.fill"]
+    let columns = [
+            GridItem(.adaptive(minimum: 56))
+        ]
+
     var body: some View {
-        VStack {
-            ForEach(0..<3) { _ in
-                HStack {
-                    ForEach(0..<6) { _ in
-                        Text("oi")
-                        
+//        VStack {
+//            ForEach(icons, id: \.self) { icon in
+//                HStack {
+        LazyVGrid(columns: columns, spacing: 20) {
+                    ForEach(icons, id: \.self) { icon in
+                        Button {
+                            print("oi")
+                        } label: {
+                            Label("", systemImage: icon)
+                            .foregroundColor(Color(.white))
+                            .font(.custom("SFProText-Regular", size: 22))
+                            .padding(.leading, 8)
+                            
+                        }
+   
                     }.frame(width: UIScreen.screenWidth/7, height: UIScreen.screenWidth/7)
                         .background(                Color(EnumColors.essenciaisColor.rawValue))
                             .cornerRadius(10)
 
-                }
-            }.listRowBackground(Color.clear)
+        }.padding(.leading)
+            .padding(.trailing)
+            .listRowBackground(Color.clear)
+            }
         }
-    }
-}
+//    }
+//}
 
 struct ModalView_Previews: PreviewProvider {
     static var previews: some View {
