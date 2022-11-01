75385d43204937aaffa3b635ecc9616b6a143c33
Gabriele Namie <gabrielenamie@hotmail.com>
2022-11-01 18:01:54 -0300

    tela método gastos pronta

---
+++ Macro/Macro/Model/Enum/EnumMethodology.swift
@@ -0,0 +1,25 @@
+//
+//  EnumMethodology.swift
+//  Macro
+//
+//  Created by Gabriele Namie on 01/11/22.
+//
+
+import SwiftUI
+
+enum EnumMethodology: String {
+    case imageOne = "essencialsImage"
+    case titleOne = "Gastos essenciais"
+    case descriptionOne = "Foque 50% da sua renda com os gastos que não podem ser adiados."
+    case exampleOne = "Ex.: Se sua renda for de R$3000 utilize R$1500 para pagar as contas mensais da sua casa."
+    
+    case imageTwo = "priorityImage"
+    case titleTwo = "Prioridades financeiras"
+    case descriptionTwo = "Priorize 35% da sua renda com dívidas de cartão e outros tipos de endividamentos."
+    case exampleTwo = "Ex.: Se sua renda for de R$3000 utilize R$1050 para pagar suas dívidas do cartão de crédito."
+    
+    case imageThree = "leisureImage"
+    case titleThree = "Estilo de vida"
+    case descriptionThree = "Aproveite 15% de sua renda com atividades e produtos que te fazem feliz!"
+    case exampleThree = "Ex.: Se sua renda for de R$3000 utilize R$450 para comprar fazer suas compras na loja."
+}
---
+++ Macro/Macro/View/Settings/elements/MethodologyGoalsView.swift
@@ -0,0 +1,20 @@
+//
+//  MethodologyGoalsView.swift
+//  Macro
+//
+//  Created by Gabriele Namie on 01/11/22.
+//
+
+import SwiftUI
+
+struct MethodologyGoalsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct MethodologyGoalsView_Previews: PreviewProvider {
+    static var previews: some View {
+        MethodologyGoalsView()
+    }
+}
