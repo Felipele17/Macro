5ece1dc957c88afe40636f6a14320561f2d58732
Gabriele Namie <gabrielenamie@hotmail.com>
2022-11-22 14:34:42 -0300

    button save

--- Macro/Macro/View/Formulary/ModalView.swift
+++ Macro/Macro/View/Formulary/ModalView.swift
@@ -10,32 +10,40 @@ import SwiftUI
 struct ModalView: View {
     @Binding var selectedIcon: String
     var colorIcon: String
+    @State var selected: Bool = false
     var icons = ["car.fill", "books.vertical.fill", "house.fill", "iphone", "graduationcap.fill", "pawprint.fill", "cart.fill", "ticket.fill", "creditcard.fill", "airplane", "pills.fill", "gamecontroller.fill", "cup.and.saucer.fill", "fork.knife", "gift.fill", "lightbulb.fill", "bag.fill", "cross.case.fill"]
     let columns = [
         GridItem(.adaptive(minimum: 56))
     ]
     
     var body: some View {
-        LazyVGrid(columns: columns, spacing: 20) {
-            ForEach(icons, id: \.self) { icon in
-                Button {
-                    selectedIcon = icon
-                } label: {
-                    Label("", systemImage: icon)
-                        .foregroundColor(Color(.white))
-                        .font(.custom("SFProText-Regular", size: 22))
-                        .padding(.leading, 8)
+        VStack {
+            LazyVGrid(columns: columns, spacing: 20) {
+                ForEach(icons, id: \.self) { icon in
+                    Button {
+                        selectedIcon = icon
+                        selected = true
+                    } label: {
+                        Label("", systemImage: icon)
+                            .foregroundColor(Color(.white))
+                            .font(.custom("SFProText-Regular", size: 22))
+                            .padding(.leading, 8)
+                        
+                    }.frame(width: UIScreen.screenWidth/7, height: UIScreen.screenWidth/7)
+                        .background(selectedIcon == icon ? Color(EnumColors.buttonColor.rawValue) : Color(colorIcon))
+                        .cornerRadius(10)
                     
-                }.frame(width: UIScreen.screenWidth/7, height: UIScreen.screenWidth/7)
-                    .background(selectedIcon == icon ? Color(EnumColors.buttonColor.rawValue) : Color(colorIcon))
-                    .cornerRadius(10)
-                if selectedIcon == icon {
-                        Text("Salvo!")
-                    }
+                }
+            }.padding(.leading)
+                .padding(.trailing)
+                .listRowBackground(Color.clear)
+            if selected {
+                Text("Salvo!")
+                    .font(.custom(EnumFonts.semibold.rawValue, size: 22))
+                    .padding(.top, 40)
+                    .foregroundColor(Color(EnumColors.title.rawValue))
             }
-        }.padding(.leading)
-            .padding(.trailing)
-            .listRowBackground(Color.clear)
+        }
     }
 }
 
